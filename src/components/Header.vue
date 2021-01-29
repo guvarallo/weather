2c1dba1f692be48b7a6ce37386167159c854fa1e
Gustavo Varallo <gustavo.varallo@live.com>
2021-01-29 11:34:16 -0300

    add logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,10 @@
   <div
     class="flex items-center mt-0 h-20 bg-gradient-to-b from-purple-600 to-blue-600"
   >
-    <h1 class="text-white text-3xl ml-5">
+    <img src="../assets/logo.png" alt="weather logo" class="w-10 ml-8" />
+    <h1 class="text-white text-3xl ml-2">
       Weather
     </h1>
-    <p class="text-white text-lg mr-5 ml-auto">Language: EN | °C</p>
+    <p class="text-white text-lg mr-8 ml-auto">EN | °C</p>
   </div>
 </template>
